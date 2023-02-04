2d19192adb458597156dfcd6d4959759fbfa6564
tluwaniMS <tluwani2020@gmail.com>
2023-02-04 10:11:05 +0200

    final-touches-to-template-set-up

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import { app } from './app.js'
 import { config } from 'dotenv'
+import mongoose from 'mongoose'
 
 config()
 
 const port = process.env.PORT || 3002
+const mongo_url = process.env.MONGO_URI
 
+mongoose.connect(mongo_url).then(() => {
+  console.log('Database connection secured.')
+})
 app.listen(port, () => {
   console.log(`server is running on : http://localhost:${port}/`)
 })
